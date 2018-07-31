3024f521a6fc5fd60cc22d4f17cd3033408140a1
balu13 <balu13@gmail.com>
2018-08-01 02:08:35 +0300

    bus stops done

--- Week2/busStop.cpp
+++ Week2/busStop.cpp
@@ -1,13 +1,107 @@
 #include <iostream>
+#include <string>
 #include <map>
+#include <vector>
 
 using namespace std;
 
+int printVector(const vector<string>& vec, const string exclusion)
+{
+	int printedCount = 0;
+	for (auto elem : vec)
+	{
+		if (exclusion != elem)
+		{
+			++printedCount;
+			cout << elem << " ";
+		}
+	}
+
+	return printedCount;
+}
+
 int main()
 {
+	int commandNum;
+	cin >> commandNum;
 
-	cin.clear();
-	cin.ignore(32767, '\n');
-	cin.get();
-	return 0;
+	map<string, vector<string>> busStops;
+	map<string, vector<string>> stopBuses;
+
+	for (int commandNo = 0; commandNo<commandNum; ++commandNo)
+	{
+		string command;
+		cin >> command;
+
+		if (command == "NEW_BUS")
+		{
+			string busName;
+			int stopNum;
+			cin >> busName >> stopNum;
+
+			for (int stopNo = 0; stopNo < stopNum; ++stopNo)
+			{
+				string stopName;
+				cin >> stopName;
+				busStops[busName].push_back(stopName);
+				stopBuses[stopName].push_back(busName);
+			}
+
+		}
+		else if (command == "BUSES_FOR_STOP")
+		{
+			string stopName;
+			cin >> stopName;
+
+			if (stopBuses.count(stopName)==0)
+			{
+				cout << "No stop" << endl;
+			}
+			else
+			{
+				printVector(stopBuses[stopName], "");
+				cout << endl;
+			}
+		}
+		else if (command == "STOPS_FOR_BUS")
+		{
+			string busName;
+			cin >> busName;
+
+			if (busStops.count(busName) == 0)
+			{
+				cout << "No bus" << endl;
+			}
+			else
+			{
+				for (auto stopName : busStops[busName])
+				{
+					cout << "Stop " << stopName << ": ";
+					int busNum = printVector(stopBuses[stopName], busName);
+					if (busNum == 0)
+					{
+						cout << "no interchange";
+					}
+					cout << endl;
+				}
+			}
+		}
+		else if (command == "ALL_BUSES")
+		{
+			if (busStops.empty())
+			{
+				cout << "No buses" << endl;
+			}
+			else
+			{
+				for (auto busStop : busStops)
+				{
+					cout << "Bus " << busStop.first << ": ";
+					printVector(busStop.second, "");
+					cout << endl;
+				}
+			}
+		}
+	}
+
 }
