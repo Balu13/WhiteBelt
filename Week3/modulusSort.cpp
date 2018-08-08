b1347b02403b686f855aefb45d756cc108524fd2
balu13 <balu13@gmail.com>
2018-08-08 16:38:16 +0300

    modulus sort solution

---
+++ Week3/modulusSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int number;
+	cin >> number;
+
+	vector<int> numbers;
+	for (int i = 0; i < number; ++i)
+	{
+		int num;
+		cin >> num;
+		numbers.push_back(num);
+	}
+
+	sort(begin(numbers), end(numbers), [](const auto& first, const auto& second)
+	{
+		return abs(first) < abs(second);
+	});
+
+	for (const auto& el : numbers)
+	{
+		cout << el << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
